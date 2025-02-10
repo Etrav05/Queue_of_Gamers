0e788aa56d82bd445e2f423e0d137d75c0375941
Etrav05 <Etravis090705@gmail.com>
2025-02-10 16:58:45 -0500

    - Created queue and user structs - Function to inizailze queue           -  Uses command line arg - Functions to en & dequeue

--- Gamer_Queue/main.c
+++ Gamer_Queue/main.c
@@ -1,21 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define NAME_LEN			10
+#define NUM_OF_USERS		20
 
 // Evan Travis - Prog72370xW25 - assignment 1 - queues
 
-// - Create a struct to represent users in a game [ ]
-// - Each person has unique username              [ ]
-// - their level (levels can range from 1 – 60)   [ ]
-// - their faction (red, blue or green)           [ ]
+// - Create a struct to represent users in a game [X]
+// - Each person has unique username              [X]
+// - their level (levels can range from 1 – 60)   [X]
+// - their faction (red, blue or green)           [X]
 
-// - The queue should be created as a struct containing two node pointers : for the head and tail of the queue [ ]
-// - The queue should be created from a set of linked nodes											           [ ]
-// Use any code from week04 (or before).The queue will require at minimum :
+// - The queue should be created as a struct containing two node pointers : for the head and tail of the queue [X]
+// - The queue should be created from a set of linked nodes											           [X]
+// - Use any code from week04 (or before).The queue will require at minimum :
 
-// - A function to initialize a blank queue    [ ]
-// - A function to check if the queue is empty [ ]
-// - A function to enqueue a node              [ ]
-// - A function to dequeue a node              [ ]
-// - Create a struct to represent a queue node [ ]
-// - This structure contains a node pointer and a user struct as its data [ ]
+//		- A function to initialize a blank queue    [X]
+//		- A function to check if the queue is empty [X]
+//		- A function to enqueue a node              [X]
+//		- A function to dequeue a node              [X]
+//		- Create a struct to represent a queue node [ ]
+//		- This structure contains a node pointer and a user struct as its data [ ]
 
 // - Write a function to enqueue the queue with a number of randomly created users [ ]
 // - You will need to pass this function a pointer to an initialized queue         [ ]
@@ -32,3 +37,72 @@
 
 // for random colours -> make an array of the colours and get a random number 0-2 to chose the index [ ]
 // properties -> debugging -> command line arg -> number of users you want                           [ ]
+
+typedef struct user {
+	char name[NAME_LEN];
+	char faction[5];
+	int level;
+} USER;
+
+typedef struct queue {
+	USER* arr;	  // array of users
+	int head;	  // first in line
+	int tail;     // back of the line
+	int capacity; // maximum capacity of the queue
+	int size;     // current number of elements in the queue
+}QUEUE;
+
+bool isEmpty(QUEUE* q) { 
+	return (q->head == q->tail - 1); // this will return -1 if the queue is empty (because head and tail will == -1)
+}
+
+bool isFull(QUEUE* q) { 
+	return (q->tail == NUM_OF_USERS); 
+}
+
+USER enqueue(QUEUE* q, USER u) {
+	if (isFull(q)) { // check if the queue is full
+		printf("Queue is full\n");
+		return;
+	}
+
+	q->arr[q->tail] = u; // if not, then add user to queue at the tail
+	q->tail = (q->tail + 1); // % q->capacity; // move the tail pointer
+	q->size++; // move the tail pointer to the next item
+}
+
+USER dequeue(QUEUE* q) {
+	USER emptyUser = { "", "", 0}; // default empty User to return
+
+	if (isEmpty(q)) { // check if the queue is empty 
+		printf("Queue is empty\n");
+		return emptyUser;
+	}
+
+	USER user = q->arr[q->head]; // take the item at the front of the queue
+	q->head++; // move the head pointer to the next item
+
+	return user; // return item that was dequeued
+}
+
+void initializeQueue(QUEUE* q, int capacity) {
+	q->capacity = capacity;
+	q->arr = (USER*)malloc(capacity * sizeof(USER)); // allocate memory for the queue
+
+	if (q->arr == NULL) { // error handling
+		printf("Problem occured while allocating memory\n");
+		exit(1); // exit the if the memory allocation fails
+	}
+
+	q->head = -1;
+	q->tail = 0;
+	q->size = 0;
+}
+
+int main(int argc, char* argv[]) { // accept commandlin args (this will be the amount of users) --> 20)
+	QUEUE q;
+	int capacity = atoi(argv[1]); // the command line arg (20) = capacity
+
+	initializeQueue(&q, capacity);
+
+}
